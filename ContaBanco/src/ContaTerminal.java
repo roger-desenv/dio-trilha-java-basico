271bbd75dad2192395002d98955eb7cb4f226cb5
Roger Costa <roger.desenv@gmail.com>
2024-10-01 21:52:09 -0300

    parametros criados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        
+        System.out.println("Por favor, digite o número da Agência!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite sua conta!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome!");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Qual seu saldo bancário?");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Muito Obrigado!");
     }
 }
