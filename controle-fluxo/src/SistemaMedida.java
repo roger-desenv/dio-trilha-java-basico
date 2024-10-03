61aff18017a43223615ca9f1dabb0369fed3d3f4
Roger Costa <roger.desenv@gmail.com>
2024-10-03 17:48:15 -0300

    switch case

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,28 @@
+public class SistemaMedida {
+  public static void main(String[] args) {
+    
+    String sigla = "R";
+
+    switch (sigla) {
+      case "P": {
+        System.out.println("PEQUENO");
+        break;
+      }
+
+      case "G": {
+        System.out.println("GRANDE");
+        break;
+      }
+
+      case "M": {
+        System.out.println("MÉDIO");
+        break;
+      }
+    
+      default:
+        System.out.println("INDEFINIDO");
+        break;
+    }
+
+  }
+}
