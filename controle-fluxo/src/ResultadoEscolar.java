ef5ab13ed2ea6d64353780c118d654691894a92f
Roger Costa <roger.desenv@gmail.com>
2024-10-02 21:38:32 -0300

    inserindo condicional

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+public class ResultadoEscolar {
+  public static void main(String[] args) {
+    
+    double nota = 7.2;
+
+    if(nota >=7)
+    System.out.println("Aprovado!");
+    
+    else
+    System.out.println("Reprovado!");
+    
+
+  }
+}
