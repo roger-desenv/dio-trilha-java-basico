a00bd09c0bd806ee3851c5748597a9343f4c3de3
Roger Costa <roger.desenv@gmail.com>
2024-10-03 00:17:19 -0300

    código limpo

---
+++ controle-fluxo/src/ResultadoEscolarLimpo.java
@@ -0,0 +1,9 @@
+public class ResultadoEscolarLimpo {
+  public static void main(String[] args) {
+    
+    int nota = 8;
+    String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+    System.out.println(resultado);
+
+  }
+}
