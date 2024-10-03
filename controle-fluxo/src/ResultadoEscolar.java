0a16a43088efb0c43503420e9edcfa1f559c1c03
Roger Costa <roger.desenv@gmail.com>
2024-10-03 00:08:37 -0300

    implementando condições

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,21 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
     
-    double nota = 7.2;
+    double nota = 7;
 
-    if(nota >=7){
+    if(nota >=7 && nota <8){
       System.out.println("Aprovado!");
     
-    } else
-      System.out.println("Reprovado!");
+    } else if (nota >=8 && nota <=10) {
+      System.out.println("Você é um Gênio!");
+
+
+    } else if (nota >=5 && nota <=7) {
+      System.out.println("Recuperação!");
     
+    } else
+     System.out.println("Reprovado!");
+      
 
   }
 }
