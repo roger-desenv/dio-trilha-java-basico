bc18392425df5103b91e0adfd874c69483b20282
Roger Costa <roger.desenv@gmail.com>
2024-10-03 22:00:43 -0300

    outro exemplo de for

--- controle-fluxo/src/ExemploFor.java
+++ controle-fluxo/src/ExemploFor.java
@@ -1,9 +1,15 @@
 public class ExemploFor {
   public static void main(String[] args) {
    
-      for (int carneirinhos = 1; carneirinhos <=20; carneirinhos ++){
+    /* 
+     for (int carneirinhos = 1; carneirinhos <=20; carneirinhos ++){
       System.out.println("Contando carneirinhos" + carneirinhos);
     }
       System.out.println("Joãozinho Dormiu!");
+  }*/
+    for ( int dia = 1; dia <=30; dia ++){
+      System.out.println("Dia " + dia);
+    }
+    System.out.println("Fim do mês!");
   }
 }
