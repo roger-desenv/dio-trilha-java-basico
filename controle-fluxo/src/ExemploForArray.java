bc6c39381364ab5d7e7c7b2ae10447d444870565
Roger Costa <roger.desenv@gmail.com>
2024-10-06 15:55:29 -0300

    estudando arrays

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,16 @@
+public class ExemploForArray {
+  public static void main(String[] args) {
+    String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+
+    System.out.println(alunos.length);
+
+    for (int x=0; x<alunos.length; x++) {
+      System.out.println("O aluno no indice x=" + x + " é " + alunos [x]);
+    }
+
+    for (String aluno: alunos){
+      System.out.println("O nome do aluno é " + aluno);
+    }
+  }
+}
